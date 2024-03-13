f120b4feff5e5098a915851894d1fd94f303359c
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-03-13 16:25:25 +0900

    Using Types

--- section2/understanding-ts/app.js
+++ section2/understanding-ts/app.js
@@ -1,1 +1,7 @@
-console.log("your typescript code!...");
+function add(number1, number2) {
+    return number1 + number2;
+}
+var number1 = 4;
+var number2 = 3.5;
+var result = add(number1, number2);
+console.log(result);
--- section2/understanding-ts/app.ts
+++ section2/understanding-ts/app.ts
@@ -1,1 +1,10 @@
-console.log("your typescript code!...");
+function add(number1: number, number2: number) {
+  return number1 + number2;
+}
+
+const number1 = 4;
+const number2 = 3.5;
+
+const result = add(number1, number2);
+
+console.log(result);
