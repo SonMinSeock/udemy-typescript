6f08502c1fa23874b317d80d8da00e56f24cec6a
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-03-14 16:27:48 +0900

    열거형으로 작업하기

--- section2/understanding-ts/app.js
+++ section2/understanding-ts/app.js
@@ -5,18 +5,39 @@
 //   name: "Son MinSeock",
 //   age: 26,
 // };
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: "Son MinSeock",
+//   age: 26,
+//   hobbies: ["Sports", "Cooking"],
+//   role: [2, "author"],
+// };
+// person.role.push("admin");
+// person.role[1] = 10;
+// person.role = [ 3, 'author', 10];
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
 var person = {
     name: "Son MinSeock",
     age: 26,
     hobbies: ["Sports", "Cooking"],
-    role: [2, "author"],
+    role: Role.AUTHOR,
 };
-person.role.push("admin");
-// person.role[1] = 10;
 var favoriteActivities;
 favoriteActivities = ["Sports"];
 console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
+}
+if (person.role === Role.AUTHOR) {
+    console.log("is author");
 }
--- section2/understanding-ts/app.ts
+++ section2/understanding-ts/app.ts
@@ -6,22 +6,35 @@
 //   age: 26,
 // };
 
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
-  name: "Son MinSeock",
-  age: 26,
-  hobbies: ["Sports", "Cooking"],
-  role: [2, "author"],
-};
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: "Son MinSeock",
+//   age: 26,
+//   hobbies: ["Sports", "Cooking"],
+//   role: [2, "author"],
+// };
 
 // person.role.push("admin");
 // person.role[1] = 10;
 
 // person.role = [ 3, 'author', 10];
+
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
+const person = {
+  name: "Son MinSeock",
+  age: 26,
+  hobbies: ["Sports", "Cooking"],
+  role: Role.AUTHOR,
+};
 
 let favoriteActivities: string[];
 
@@ -31,4 +44,8 @@ console.log(person.name);
 
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
+}
+
+if (person.role === Role.AUTHOR) {
+  console.log("is author");
 }
