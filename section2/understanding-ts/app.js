2a111e5be81379561e857f983ee5c16c54faec11
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-03-14 15:43:10 +0900

    배열 타입

--- section2/understanding-ts/app.js
+++ section2/understanding-ts/app.js
@@ -1,5 +1,19 @@
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+//   name: "Son MinSeock",
+//   age: 26,
+// };
 var person = {
     name: "Son MinSeock",
     age: 26,
+    hobbies: ["Sports", "Cooking"],
 };
+var favoriteActivities;
+favoriteActivities = ["Sports"];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- section2/understanding-ts/app.ts
+++ section2/understanding-ts/app.ts
@@ -9,6 +9,15 @@
 const person = {
   name: "Son MinSeock",
   age: 26,
+  hobbies: ["Sports", "Cooking"],
 };
 
+let favoriteActivities: string[];
+
+favoriteActivities = ["Sports"];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
