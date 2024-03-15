966d96f68fbb81bb433ab394acd804c126833399
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-03-15 18:41:15 +0900

    리터럴 타입 실습 코드를 컴파일 했음.

--- section2/understanding-ts/app.js
+++ section2/understanding-ts/app.js
@@ -1,14 +1,16 @@
-function combine(input1, input2) {
+function combine(input1, input2, resultConversion) {
     var result;
-    if (typeof input1 === "number" && typeof input2 === "number") {
-        result = input1 + input2;
+    if ((typeof input1 === "number" && typeof input2 === "number") || resultConversion === "as-number") {
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
     }
     return result;
 }
-var combinedAge = combine(10, 20);
+var combinedAge = combine(10, 20, "as-number");
 console.log(combinedAge);
-var combinedNames = combine("Max", "Jane");
+var combinedStringAges = combine("15", "10", "as-number");
+console.log(combinedStringAges);
+var combinedNames = combine("Max", "Jane", "as-text");
 console.log(combinedNames);
