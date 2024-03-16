ab07f17d9dc9604ee63b60759a62dd00a124d9ef
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-03-16 14:13:28 +0900

    "알 수 없는" 타입(unknown 타입)

--- section2/understanding-ts/app.js
+++ section2/understanding-ts/app.js
@@ -1,16 +1,7 @@
-function add(n1, n2) {
-    return n1 + n2;
+var userInput;
+var userName;
+userInput = 10;
+userInput = "son";
+if (typeof userInput === "string") {
+    userName = userInput;
 }
-function printResult(num) {
-    console.log("Result : " + num);
-}
-function addAndHandler(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-printResult(add(20, 30));
-addAndHandler(10, 30, function (result) { return console.log(result); });
-// let combineValues: (a: number, b: number) => number;
-// combineValues = add;
-// combineValues = printResult;
-// console.log(combineValues(2, 10));
--- section2/understanding-ts/app.ts
+++ section2/understanding-ts/app.ts
@@ -1,23 +1,9 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+let userInput: unknown;
+let userName: string;
+
+userInput = 10;
+userInput = "son";
+
+if (typeof userInput === "string") {
+  userName = userInput;
 }
-
-function printResult(num: number): void {
-  console.log("Result : " + num);
-}
-
-function addAndHandler(n1: number, n2: number, cb: (result: number) => void) {
-  const result = n1 + n2;
-  cb(result);
-}
-
-printResult(add(20, 30));
-
-addAndHandler(10, 30, (result) => console.log(result));
-
-// let combineValues: (a: number, b: number) => number;
-// combineValues = add;
-
-// combineValues = printResult;
-
-// console.log(combineValues(2, 10));
