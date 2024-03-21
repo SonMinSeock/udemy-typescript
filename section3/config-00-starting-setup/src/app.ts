5d14634990134f2eb62b7c41e60d57a248b42481
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-03-21 18:38:56 +0900

    코드 품질 옵션

--- section3/config-00-starting-setup/src/app.ts
+++ section3/config-00-starting-setup/src/app.ts
@@ -1,6 +1,16 @@
+let appId = 23;
 const button = document.querySelector("button")!;
 
+function add(n1: number, n2: number) {
+  if (n1 + n2 > 0) {
+    return n1 + n2;
+  }
+  return;
+}
+
 function clickHandler(message: string) {
+  let username = "son";
+
   console.log("Click! " + message);
 }
 
