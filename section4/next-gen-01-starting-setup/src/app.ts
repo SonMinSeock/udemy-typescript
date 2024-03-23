4cad7470c39f49c296ae41aec7105e6287eb34cf
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-03-23 14:51:02 +0900

    let 및 const

---
+++ section4/next-gen-01-starting-setup/src/app.ts
@@ -0,0 +1,17 @@
+const userName = "Son MinSeock";
+// userName = 'Son';
+
+let age = 25;
+
+// function add(a: number, b: number) {
+//   let result = a + b;
+//   return result;
+// }
+
+// console.log(result);
+
+// if (age > 20) {
+//   var isOld = true;
+// }
+
+// console.log(isOld);
