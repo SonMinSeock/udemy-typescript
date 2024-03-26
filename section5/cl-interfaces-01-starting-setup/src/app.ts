d065f21541721850d15508388f6522a9254ce053
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-03-26 18:23:47 +0900

    "읽기전용" 속성

--- section5/cl-interfaces-01-starting-setup/src/app.ts
+++ section5/cl-interfaces-01-starting-setup/src/app.ts
@@ -2,13 +2,14 @@ class Department {
     // private id: string;
     // private name: string;
     private employees: string[] = [];
-    constructor(private id: string, public name: string) {
+    constructor(private readonly id: string, public name: string) {
         
     }
     describe(this: Department) {
         console.log(`Department(${this.id}): ${this.name}`);
     }
     addEmployee(employee: string) {
+        // this.id = 'd11';
         this.employees.push(employee);
     }
     printEmployeeInformation() {
