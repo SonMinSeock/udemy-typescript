d209e5595491e2344aa8355800a7c909350bd7cf
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-03-26 17:49:59 +0900

    개인 및 "공용" 액세스 수정자

--- section5/cl-interfaces-01-starting-setup/src/app.ts
+++ section5/cl-interfaces-01-starting-setup/src/app.ts
@@ -1,15 +1,32 @@
 class Department {
     name: string;
+    private employees: string[] = [];
     constructor(name: string) {
         this.name = name;
     }
     describe(this: Department) {
         console.log(`Department: ${this.name}`);
     }
+    addEmployee(employee: string) {
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
 }
 
-const accounting = new Department("Accounting");
-const accountingCopy = { name: "DUMMY", describe: accounting.describe };
 
+const accounting = new Department("Accounting");
 accounting.describe();
-accountingCopy.describe();
+
+accounting.addEmployee('Son');
+accounting.addEmployee('Max');
+
+// accounting.employees[1] = 'Jane';
+
+accounting.printEmployeeInformation();
+
+
+// const accountingCopy = { name: "DUMMY", describe: accounting.describe };
+// accountingCopy.describe();
