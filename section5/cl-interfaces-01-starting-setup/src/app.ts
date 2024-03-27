8edecf57cc9b219da8d673c99c4c874c8738095c
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-03-27 19:15:05 +0900

    속성 및 "보호된" 수정자 재정의(protected 접근제한)

--- section5/cl-interfaces-01-starting-setup/src/app.ts
+++ section5/cl-interfaces-01-starting-setup/src/app.ts
@@ -1,55 +1,63 @@
 class Department {
-    // private id: string;
-    // private name: string;
-    private employees: string[] = [];
-    constructor(private readonly id: string, public name: string) {
-        
-    }
-    describe(this: Department) {
-        console.log(`Department(${this.id}): ${this.name}`);
-    }
-    addEmployee(employee: string) {
-        // this.id = 'd11';
-        this.employees.push(employee);
-    }
-    printEmployeeInformation() {
-        console.log(this.employees.length);
-        console.log(this.employees);
-    }
+  // private id: string;
+  // private name: string;
+  protected employees: string[] = [];
+  constructor(private readonly id: string, public name: string) {}
+  describe(this: Department) {
+    console.log(`Department(${this.id}): ${this.name}`);
+  }
+  addEmployee(employee: string) {
+    // this.id = 'd11';
+    this.employees.push(employee);
+  }
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 class ITDepartment extends Department {
-    public admins: string[];
-    constructor(id: string, admins: string[]) {
-        super(id, 'IT');
-        this.admins = admins;
-    }
+  public admins: string[];
+  constructor(id: string, admins: string[]) {
+    super(id, "IT");
+    this.admins = admins;
+  }
 }
 
 class AccountingDepartment extends Department {
-    constructor(id: string, private reports: string[]) {
-        super(id, "Accounting");
+  constructor(id: string, private reports: string[]) {
+    super(id, "Accounting");
+  }
+  addEmployee(name: string): void {
+    if (name === "Son") {
+      return;
     }
-    addReport(text: string) {
-        this.reports.push(text);
-    }
-    printReport() {
-        console.log(this.reports)
-    }
+
+    this.employees.push(name);
+  }
+  addReport(text: string) {
+    this.reports.push(text);
+  }
+  printReport() {
+    console.log(this.reports);
+  }
 }
 
 const it = new ITDepartment("d1", ["Son"]);
 it.describe();
 
-it.addEmployee('Son');
-it.addEmployee('Max');
+it.addEmployee("Son");
+it.addEmployee("Max");
 
 it.printEmployeeInformation();
 
-console.log(it)
+console.log(it);
 
-const accounting = new AccountingDepartment('d2', []);
+const accounting = new AccountingDepartment("d2", []);
 
-accounting.addReport('Something went wrong!');
-
+accounting.addReport("Something went wrong!");
+accounting.addEmployee("Son");
+accounting.printEmployeeInformation();
+accounting.addEmployee("Jane");
 accounting.printReport();
+accounting.printEmployeeInformation();
