5d77980c7068988cdf1977f6dbcf0dc78c110949
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-03-30 16:35:08 +0900

    클래스와 인터페이스 사용하기

--- section5/cl-interfaces-11-basic-interfaces/src/app.ts
+++ section5/cl-interfaces-11-basic-interfaces/src/app.ts
@@ -1,17 +1,20 @@
-interface Person {
+interface Greetable {
   name: string;
-  age: number;
   greet(phrase: string): void;
 }
 
-let user1: Person;
-
-user1 = {
-  name: "Son MinSeock",
-  age: 25,
-  greet(phrase) {
+class Person implements Greetable {
+  name: string;
+  age: number;
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+  greet(phrase: string) {
     console.log(`${phrase} ${this.name}`);
-  },
-};
+  }
+}
+
+const user1: Greetable = new Person("Son MinSeock", 25);
 
 user1.greet("Hello");
