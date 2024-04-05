c7e817e5fdc2e1605c05ecfd643abdd1320ef6e1
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-04-05 14:50:52 +0900

    인터섹션(교차) 타입

---
+++ section6/adv-types-01-starting-setup/src/app.ts
@@ -0,0 +1,43 @@
+type Admin = {
+  name: string;
+  privileges: string[];
+};
+
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
+  name: "손민석",
+  privileges: ["create-server"],
+  startDate: new Date(),
+};
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
+
+/*
+// 인터페이스 방식
+interface Admin {
+  name: string;
+  privileges: string[];
+}
+
+interface Employee {
+  name: string;
+  startDate: Date;
+}
+
+interface ElevatedEmployee extends Admin, Employee {}
+
+const e1: ElevatedEmployee = {
+  name: "손민석",
+  privileges: ["create-server"],
+  startDate: new Date(),
+};
+*/
