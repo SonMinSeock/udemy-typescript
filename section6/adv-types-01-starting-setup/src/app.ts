0d8c269debe4d6f118eef14dc213933eccac61cf
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-04-06 10:26:00 +0900

    구별된 유니언

--- section6/adv-types-01-starting-setup/src/app.ts
+++ section6/adv-types-01-starting-setup/src/app.ts
@@ -1,78 +1,28 @@
-type Admin = {
-  name: string;
-  privileges: string[];
-};
-
-type Employee = {
-  name: string;
-  startDate: Date;
-};
-
-type ElevatedEmployee = Admin & Employee;
-
-const e1: ElevatedEmployee = {
-  name: "손민석",
-  privileges: ["create-server"],
-  startDate: new Date(),
-};
-
-type Combinable = string | number;
-type Numeric = number | boolean;
-
-type Universal = Combinable & Numeric;
-
-type UnknownEmployee = Admin | Employee;
-
-function printEmployeeInformation(emp: UnknownEmployee) {
-  console.log("Name : ", emp.name);
-  if ("privileges" in emp) {
-    console.log("Privileges : ", emp.privileges);
-  }
-  if ("startDate" in emp) {
-    console.log("Start Date : ", emp.startDate);
-  }
+interface Bird {
+  type: "bird";
+  flyingSpeed: number;
 }
 
-printEmployeeInformation(e1);
+interface Horse {
+  type: "horse";
+  runningSpeed: number;
+}
 
-class Car {
-  drive() {
-    console.log("Drivng a car...");
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+  let speed;
+  switch (animal.type) {
+    case "bird":
+      speed = animal.flyingSpeed;
+      break;
+    case "horse":
+      speed = animal.runningSpeed;
+      break;
   }
+
+  console.log(`Moving with speed: ${speed}`);
 }
 
-interface Truck {
-  drive: () => void;
-  loadCargo: (amount: number) => void;
-}
-
-// class Truck {
-//   drive() {
-//     console.log("Drivng a truck...");
-//   }
-//   loadCargo(amount: number) {
-//     console.log("Loading cargo... " + amount);
-//   }
-// }
-
-type Vehicle = Car | Truck;
-
-const v1 = new Car();
-const v2: Truck = {
-  drive() {
-    console.log("Drivng a truck...");
-  },
-  loadCargo(amount) {
-    console.log("Loading cargo... " + amount);
-  },
-};
-
-function useVehicle(vehicle: Vehicle) {
-  vehicle.drive();
-  if ("loadCargo" in vehicle) {
-    vehicle.loadCargo(10000);
-  }
-}
-
-useVehicle(v1);
-useVehicle(v2);
+moveAnimal({ type: "bird", flyingSpeed: 10 });
+moveAnimal({ type: "horse", runningSpeed: 100 });
