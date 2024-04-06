cdaad5f6bc3fbcd3dd290eeff5c81da972ca4879
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-04-06 11:30:52 +0900

    함수 오버로드

--- section6/adv-types-01-starting-setup/src/app.ts
+++ section6/adv-types-01-starting-setup/src/app.ts
@@ -47,6 +47,8 @@ const errorBag: ErrorContainer = {
 type Combinable = string | number;
 type Numeric = number | boolean;
 
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
 function add(a: Combinable, b: Combinable) {
   if (typeof a === "string" || typeof b === "string") {
     return a.toString() + b.toString();
