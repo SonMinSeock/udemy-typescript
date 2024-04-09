0ae0aa76eb9f56eb6da27847d0cde543fb413898
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-04-09 15:14:14 +0900

    제네릭 함수 생성하기

--- section7/generics-01-starting-setup/src/app.ts
+++ section7/generics-01-starting-setup/src/app.ts
@@ -1,11 +1,10 @@
-const names: Array<string> = []; // string[]
-
-const promise: Promise<string> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("This is Done!");
-  }, 2000);
-});
-
-promise.then((result) => {
-  result.split(" ");
-});
+function merge<T extends {}, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+const mergeObj1 = merge<{ name: string; hobbies?: string[] }, { age: number }>(
+  { name: "손민석", hobbies: ["스케이트보드 타기", "락밴드 음악 듣기"] },
+  { age: 25 }
+);
+const mergeObj2 = merge<{ name: string; hobbies?: string[] }, { age: number }>({ name: "전민서" }, { age: 22 });
+console.log(mergeObj1.name, mergeObj1.age, mergeObj1.hobbies);
+console.log(mergeObj2.name, mergeObj2.age);
