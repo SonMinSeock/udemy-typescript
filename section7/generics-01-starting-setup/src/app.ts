a17230161eb8a116bf4a19574903df7c8f7ab07a
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-04-09 14:31:39 +0900

    내장 제네릭 & 제네릭이란?

---
+++ section7/generics-01-starting-setup/src/app.ts
@@ -0,0 +1,11 @@
+const names: Array<string> = []; // string[]
+
+const promise: Promise<string> = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("This is Done!");
+  }, 2000);
+});
+
+promise.then((result) => {
+  result.split(" ");
+});
