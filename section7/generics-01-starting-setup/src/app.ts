07b4ce2eae8281cbee7f4535a7b8b51bdcb1f3cc
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-04-13 17:33:36 +0900

    제약 조건 작업하기

--- section7/generics-01-starting-setup/src/app.ts
+++ section7/generics-01-starting-setup/src/app.ts
@@ -1,10 +1,7 @@
-function merge<T extends {}, U>(objA: T, objB: U) {
+function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
-const mergeObj1 = merge<{ name: string; hobbies?: string[] }, { age: number }>(
-  { name: "손민석", hobbies: ["스케이트보드 타기", "락밴드 음악 듣기"] },
-  { age: 25 }
-);
-const mergeObj2 = merge<{ name: string; hobbies?: string[] }, { age: number }>({ name: "전민서" }, { age: 22 });
+const mergeObj1 = merge({ name: "손민석", hobbies: ["스케이트보드 타기", "락밴드 음악 듣기"] }, { age: 25 });
+const mergeObj2 = merge({ name: "전민서" }, { age: 22 });
 console.log(mergeObj1.name, mergeObj1.age, mergeObj1.hobbies);
 console.log(mergeObj2.name, mergeObj2.age);
