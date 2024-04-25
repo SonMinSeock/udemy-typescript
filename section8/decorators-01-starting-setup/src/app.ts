f097d313c602c6a4b489f19929d6737be739de6a
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-04-25 14:04:20 +0900

    접근자 & 메서드 & 매개변수 데코레이터

--- section8/decorators-01-starting-setup/src/app.ts
+++ section8/decorators-01-starting-setup/src/app.ts
@@ -3,11 +3,27 @@ function Log(target: any, propertyName: string | Symbol) {
   console.log(target, propertyName);
 }
 
+function Log2(target: any, name: string, descriptor: TypedPropertyDescriptor<number>) {
+  console.log("접근자 데코레이터!");
+  console.log(target, name, descriptor);
+}
+
+function Log3(target: any, name: string | Symbol, descriptor: TypedPropertyDescriptor<(tax: number) => number>) {
+  console.log("메서드 데코레이터!");
+  console.log(target, name, descriptor);
+}
+
+function Log4(target: any, name: string | Symbol, position: number) {
+  console.log("매개변수 데코레이터!");
+  console.log(target, name, position);
+}
+
 class Product {
   @Log
   title: string;
   private _price: number;
 
+  @Log2
   set price(val: number) {
     if (val > 0) {
       this._price = val;
@@ -21,7 +37,8 @@ class Product {
     this._price = p;
   }
 
-  getPriceWithTax(tax: number) {
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
     return this._price * (1 + tax);
   }
 }
