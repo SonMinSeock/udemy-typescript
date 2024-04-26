b8d99b52f742d594a182313aa64c322dff5ae80e
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-04-26 17:47:32 +0900

    기타 데코레이터 반환 예시 - "Autobind" 데코레이터 만들기.

--- section8/decorators-01-starting-setup/src/app.ts
+++ section8/decorators-01-starting-setup/src/app.ts
@@ -74,3 +74,30 @@ class Person {
   }
 }
 const user = new Person();
+
+function AutoBind(_: any, _: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+class Printer {
+  message = "This works";
+
+  @AutoBind
+  showMessage() {
+    console.log(this.message);
+  }
+}
+
+const p = new Printer();
+
+const button = document.querySelector("button") as HTMLButtonElement;
+
+button.addEventListener("click", p.showMessage);
