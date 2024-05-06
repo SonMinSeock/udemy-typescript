1d0bf5735f77c4a09cc5950caabde2a9c443bf1b
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-05-06 20:08:48 +0900

    DOM 요소 선택 및 OOP 렌더링.

--- section9/prj-00-initial-starting-setup/src/app.ts
+++ section9/prj-00-initial-starting-setup/src/app.ts
@@ -1,1 +1,37 @@
-// Code goes here!
+// 강의에서 작성한 코드이다.
+/*
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+  constructor() {
+    this.templateElement = document.getElementById("project-input")! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+    const importedNode = document.importNode(this.templateElement.content, true);
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.attach();
+  }
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+}
+*/
+// 내가 작성한 코드.
+
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+  constructor() {
+    //this.templateElement = <HTMLTemplateElement>document.getElementById("project-input")!;
+    this.templateElement = document.getElementById("project-input")! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+    this.element = this.templateElement.content.firstElementChild as HTMLFormElement;
+    this.attach();
+  }
+  private attach() {
+    this.hostElement.appendChild(this.element);
+  }
+}
+
+const projectInput = new ProjectInput();
