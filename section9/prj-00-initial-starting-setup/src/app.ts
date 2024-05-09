73715f9a65a50d7dc7e60adaa1c5ebc3d93fd8c2
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-05-09 12:04:46 +0900

    렌더링 프로젝트 목록

--- section9/prj-00-initial-starting-setup/src/app.ts
+++ section9/prj-00-initial-starting-setup/src/app.ts
@@ -71,6 +71,33 @@ function AutoBind(_1: any, _2: string | Symbol, descriptor: PropertyDescriptor) 
   return adjDescriptor;
 }
 
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+
+  constructor(private type: "active" | "finished") {
+    this.templateElement = document.getElementById("project-list")! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+    const importedNode = document.importNode(this.templateElement.content, true);
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = `${this.type}-projects`;
+
+    this.attach();
+    this.renderContent();
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("beforeend", this.element);
+  }
+
+  private renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector("ul")!.id = listId;
+    this.element.querySelector("h2")!.textContent = `${this.type.toUpperCase()} - PROJECTS`;
+  }
+}
+
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -151,3 +178,5 @@ class ProjectInput {
 }
 
 const projectInput = new ProjectInput();
+const activePrj = new ProjectList("active");
+const finishedPrj = new ProjectList("finished");
