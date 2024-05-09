e96146382d30a75aa92a27418f89d1a1b00603fa
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-05-09 09:30:37 +0900

    사용자 입력 가져오기

--- section9/prj-00-initial-starting-setup/src/app.ts
+++ section9/prj-00-initial-starting-setup/src/app.ts
@@ -55,10 +55,38 @@ class ProjectInput {
     this.attach();
   }
 
+  private clearUserInput() {
+    this.titleInputElement.value = "";
+    this.descriptionInputElement.value = "";
+    this.peopleInputElement.value = "";
+  }
+
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+
+    if (
+      enteredTitle.trim().length === 0 ||
+      enteredDescription.trim().length === 0 ||
+      enteredPeople.trim().length === 0
+    ) {
+      alert("Invalid input, please try again!");
+      return;
+    } else {
+      return [enteredTitle, enteredDescription, +enteredPeople];
+    }
+  }
+
   @AutoBind
   private submitHandler(event: Event) {
     event.preventDefault();
-    console.log(this.titleInputElement.value, this.descriptionInputElement.value, this.peopleInputElement.value);
+    const userInput = this.gatherUserInput();
+    if (Array.isArray(userInput)) {
+      const [title, description, people] = userInput;
+      console.log(title, description, people);
+      this.clearUserInput();
+    }
   }
 
   private configure() {
