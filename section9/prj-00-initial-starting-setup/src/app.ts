b7aafae46c49cb5e3ecf1bfff1545e16c5a8ebe2
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-05-11 10:21:54 +0900

    열거형으로 프로젝트 필터링하기

--- section9/prj-00-initial-starting-setup/src/app.ts
+++ section9/prj-00-initial-starting-setup/src/app.ts
@@ -138,7 +138,14 @@ class ProjectList {
     this.element.id = `${this.type}-projects`;
 
     projectState.addListener((projects: Project[]) => {
-      this.assignedProjects = projects;
+      const relevantProjects = projects.filter((prj) => {
+        if (this.type === "active") {
+          return prj.status === ProjectStatus.Active;
+        } else {
+          return prj.status === ProjectStatus.Finished;
+        }
+      });
+      this.assignedProjects = relevantProjects;
       this.renderProjects();
     });
 
@@ -148,6 +155,7 @@ class ProjectList {
 
   private renderProjects() {
     const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
+    listEl.innerHTML = "";
     for (const prjItem of this.assignedProjects) {
       const listItem = document.createElement("li");
       listItem.textContent = prjItem.title;
