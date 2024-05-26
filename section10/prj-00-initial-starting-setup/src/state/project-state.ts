a593c5bfe20e3d3c348aca19377f13b09cbe1341
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-05-26 18:16:34 +0900

    파일 및 폴더 정리하기

---
+++ section10/prj-00-initial-starting-setup/src/state/project-state.ts
@@ -0,0 +1,53 @@
+namespace App {
+  // Listener 타입 정의.
+  type Listener<T> = (items: T[]) => void;
+
+  class State<T> {
+    protected listeners: Listener<T>[] = [];
+
+    addListener(listenerFn: Listener<T>) {
+      this.listeners.push(listenerFn);
+    }
+  }
+
+  // Project 상태 관리해주는 클래스.
+  export class ProjectState extends State<Project> {
+    // 필드 정의.
+    private projects: Project[] = [];
+    //싱글톤 패턴 적용.
+    private static instance: ProjectState;
+    private constructor() {
+      super();
+    }
+    static getInstance() {
+      if (this.instance) {
+        return this.instance;
+      }
+      this.instance = new ProjectState();
+      return this.instance;
+    }
+
+    addProject(title: string, description: string, numOfPeople: number) {
+      const newProject = new Project(Math.random().toString(), title, description, numOfPeople, ProjectStatus.Active);
+      this.projects.push(newProject);
+
+      this.updateListeners();
+    }
+
+    moveProject(prjId: string, newStatus: ProjectStatus) {
+      const project = this.projects.find((prj) => prj.id === prjId);
+      if (project && project.status !== newStatus) {
+        project.status = newStatus;
+        this.updateListeners();
+      }
+    }
+
+    private updateListeners() {
+      for (const listenerFn of this.listeners) {
+        listenerFn(this.projects.slice());
+      }
+    }
+  }
+
+  export const projectState = ProjectState.getInstance();
+}
