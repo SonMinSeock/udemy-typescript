69f69f260c2a2401e07d1859bc7633286b1f614d
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-05-26 18:20:06 +0900

    최종 파일 및 폴더 정리하기

--- section10/prj-00-initial-starting-setup/src/components/project-list.ts
+++ section10/prj-00-initial-starting-setup/src/components/project-list.ts
@@ -1,4 +1,9 @@
 /// <reference path="base-component.ts" />
+/// <reference path="../models/drag-drop.ts" />
+/// <reference path="../models/project.ts" />
+/// <reference path="../state/project-state.ts" />
+/// <reference path="../decorators/auto-bind.ts" />
+
 namespace App {
   export class ProjectList extends Component<HTMLDivElement, HTMLElement> implements DragTarget {
     assignedProjects: Project[] = [];
